0bcaa655d2b9f8eea11a3b993dda60f3c7e1ea40
ShreyashSri <shreyash.sri09@gmail.com>
2025-01-18 10:45:17 +0530

    Removed sensitive files

--- career/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Opportunity, Application, MockTest
-
-admin.site.register(Opportunity)
-admin.site.register(Application)
-admin.site.register(MockTest)
